e2d6a1fd6f9a3a49d408b25498eab239fcbc117c
Sharky <noobie2019@listingslab.com>
2021-05-23 21:55:36 +1000

    14.0.5

--- react/wp-admin/src/components/AppShell.jsx
+++ react/wp-admin/src/components/AppShell.jsx
@@ -2,13 +2,8 @@ import React from 'react'
 import clsx from 'clsx'
 import {
     makeStyles,
-    ButtonBase,
-    // Avatar,
-    Card,
-    // CardHeader,
-    // CardContent,
-    // Badge,
     Typography,
+    Grid,
 } from '@material-ui/core/'
 import { Host } from './'
 
@@ -28,18 +23,6 @@ const useStyles = makeStyles(theme => ({
 		paddingBottom: theme.spacing( 2 ),
 		textAlign: 'center',
 	},
-	btn:{
-		width: '100%',
-		display: 'block',
-		textAlign: 'left',
-	},
-	card: {
-		background: 'none',
-	    boxRadius: 'none',
-	    boxShadow: 'none',
-	    border: 'none',
-	},
-
 }))
 
 export default function AppShell( props ) {
@@ -48,40 +31,21 @@ export default function AppShell( props ) {
 
 	return	<React.Fragment>
 				<div className={ clsx( classes.appShell ) } >
-					<Typography 
-						variant={ `h4` }
-						className={ clsx( classes.title ) }
-					>
-						@listingslab plugin
-					</Typography>
-					<ButtonBase 
-						color={ `primary` }
-						className={ clsx( classes.btn ) } 
-						onClick={ (e) => {
-							e.preventDefault()
-						}}>
-						<Card className={ clsx( classes.card ) } >
+					
+					<Grid container>
+						<Grid item xs={ 12 }>
+							<Typography 
+								variant={ `h4` }
+								className={ clsx( classes.title ) }>
+								@listingslab plugin
+							</Typography>
+						</Grid>
+
+						<Grid item xs={ 6 }>
 							<Host />
-						</Card>
-					</ButtonBase>
+						</Grid>
+					</Grid>
+					
 				</div>
 			</React.Fragment>
 }
-
-/*
-
-<CardHeader 
-								title={ `Messages` }
-								subheader={ `将react app添加到WordPress管理页面` }
-								avatar={<Badge 
-											color={ `secondary` }
-											badgeContent={ 1 } >
-											<Avatar src={ `http://localhost:8888/logo.png` }/>
-										</Badge>}
-							/>
-<CardContent>
-						<Typography variant={ `body2` }>
-							{ `body` }
-						</Typography>
-					</CardContent>
-*/
