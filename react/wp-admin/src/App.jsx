bf70f7c2c2bd1a0a99caf9336f44759ff72b62b2
listingslab <listingslab@gmail.com>
2021-06-19 08:13:20 +1000

    tweak to start

--- react/wp-admin/src/App.jsx
+++ react/wp-admin/src/App.jsx
@@ -26,6 +26,6 @@ export default function App() {
   }, [apiSlice])
 
   return <MuiThemeProvider theme={createMuiTheme(theme)}>
-            <AppShell />
+            AppShell
          </MuiThemeProvider> 
 }
