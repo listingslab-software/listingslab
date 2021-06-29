306b45034a31efb71cdb14b5de26153491377663
listingslab <listingslab@gmail.com>
2021-06-29 22:14:54 +1000

    14.2.8

--- listingslab.php
+++ listingslab.php
@@ -5,7 +5,7 @@
  * @package           listingslab
  *
  * @wordpress-plugin
- * Version:           14.2.7
+ * Version:           14.2.
  * Plugin Name:       @listingslab
  * Description:       Magically turn any WordPress Site into a much superior React Progressive WordPress App
  * Plugin URI:        https://github.com/listingslab-software/listingslab
@@ -19,7 +19,7 @@
 
 
 defined( 'ABSPATH' ) or die( 'Really?' );
-define( 'listingslab_VERSION', '14.2.7' );
+define( 'listingslab_VERSION', '14.2.8' );
 
 require_once 'php/Listingslab.php';
 
